9612bb718bc33441f49f9386d7ea5876bb451a5d
monsteronfire <monsteronfire@gmail.com>
2016-10-31 01:27:51 +0800

    Add npm and webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: [
+    { site: './source/javascripts/site.js' }
+  ],
+
+  resolve: [
+    { root: __dirname + '/source/javascripts' }
+  ],
+
+  output: {
+    path: __dirname + '/.tmp/dist',
+    filename: 'javascripts/site.js'
+  }
+}
